5a5c6d53a9ff3483de14e8c608f41be73237e120
Lakshya2907 <lakshya.gupta21289@gmail.com>
2024-09-20 19:49:43 +0530

    Implemented Cors and added sql

--- Backend/ecommerce-project/src/main/java/com/example/ecommerce_project/controller/ProductController.java
+++ Backend/ecommerce-project/src/main/java/com/example/ecommerce_project/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce_project.controller;
 import com.example.ecommerce_project.model.Product;
 import com.example.ecommerce_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
@@ -22,6 +24,7 @@ public class ProductController {
         return "Hello Randi";
     }
 
+    @GetMapping("/products")
     public List<Product> getAllProducts(){
         return service.getAllProducts();
     }
--- Backend/ecommerce-project/src/main/java/com/example/ecommerce_project/model/Product.java
+++ Backend/ecommerce-project/src/main/java/com/example/ecommerce_project/model/Product.java
@@ -1,9 +1,12 @@
 package com.example.ecommerce_project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -15,13 +18,14 @@ import java.util.Date;
 @Data
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String desc;
     private String brand;
     private BigDecimal price;
     private String category;
-    private Date relaseDate;
+    private Date releaseDate;
     private Boolean available;
     private int quantity;
 }
